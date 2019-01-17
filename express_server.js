85214bb76b89bb0078bcf7c26e4db98e65e45ef6
silentscribe <seelive.i@gmail.com>
2019-01-17 20:04:10 +0000

    styling added

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 
+// Example URL Database
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
@@ -18,10 +19,11 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);
+// });
 
+// Sends the URL database and username to the index page
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
@@ -30,6 +32,7 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// For adding a new URL
 app.get('/urls/new', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
@@ -69,12 +72,14 @@ app.get('/u/:shortURL', (req, res) => {
 //   res.send('<html><body>Hello <b>World</b></body></html>\n');
 // });
 
+// when a request to delete a url this is activated.
 app.post('/urls/:id/delete', (req, res) => {
   const shortURL = req.params.id;
   delete urlDatabase[shortURL];
   res.redirect(`http://localhost:8080/urls`);
 });
 
+// when a URL is updated/changed this is activated.
 app.post('/urls/:id', (req, res) => {
   const newURL = req.body.updatedURL;
   const httpCheck = newURL.slice(0, 7);
@@ -87,13 +92,15 @@ app.post('/urls/:id', (req, res) => {
   }
 });
 
+// user logs in and cookie gets set.
 app.post('/login', (req, res) => {
   res.cookie('username', req.body.username);
   res.redirect(`http://localhost:8080/urls`);
 });
 
+// receiving a request to log out. Clears cookies then sends them back to urls page.
 app.post('/logout', (req, res) => {
-  res.clearCookie(req.cookies.username);
+  res.clearCookie('username');
   res.redirect(`http://localhost:8080/urls`);
 });
 
